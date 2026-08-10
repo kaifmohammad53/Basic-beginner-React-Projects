ff058addb6beb13c01312690586eeb958bd984b3
kaifmohammad53 <kaifmohammad9530@gmail.com>
2026-08-10 20:29:32 +0530

    adding one more project in the list of learning reacts

---
+++ 05_password_generator/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-5 flex justify-center align-center">Password Generator</div>
+    </>
+  );
+}
+
+export default App;
